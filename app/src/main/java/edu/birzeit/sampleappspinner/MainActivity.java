5f000a593f75fb2b7d23c636d994e2d504132122
Mohammad Ihraiz <mohammad.a.ihraiz@gmail.com>
2021-03-25 18:21:12 +0200

    Version 1.1
    Add items to spinner dynamically using java code

--- app/src/main/java/edu/birzeit/sampleappspinner/MainActivity.java
+++ app/src/main/java/edu/birzeit/sampleappspinner/MainActivity.java
@@ -4,15 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    private Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        spinner = findViewById(R.id.spnLanguages);
+
+        populateSpinner();
+
+    }
+
+    private void populateSpinner() {
+        ArrayList<String> data = new ArrayList<>();
+        data.add("Java");
+        data.add("C");
+        data.add("C#");
+        data.add("C++");
+        data.add("PHP");
+        data.add("Python");
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, data);
+        spinner.setAdapter(adapter);
+
     }
 
     public void btn_OnClick(View view) {
